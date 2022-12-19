cd478866b38a3821eea38f37b93f03eac1e4765d
Taslim Owolarafe <owolarafetaslim@gmail.com>
2022-12-19 10:42:47 +0100

    Update settings.py
    added ALLOWED_HOSTS = ['*']

--- LMS/settings.py
+++ LMS/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6nj1f*(l6hz=xve3l8n+7e&aezuvp-4qn(d6v%=s5t1ms2uzlv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
